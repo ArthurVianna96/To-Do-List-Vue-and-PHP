016ff68c00761752bfad5f76ce91d3642dfbf339
Arthur <arthurpedrosa96.ap@gmail.com>
2021-08-20 09:14:55 -0300

    'finished tasks container file'

---
+++ client/components/finished_tasks_container.js
@@ -0,0 +1,49 @@
+app.component('finished-tasks-container', {
+    props:{
+        finishedtasks:{
+            type: Array,   
+        }
+    },
+    data(){
+        return {
+            currentTask: {}
+        }
+    },
+    template:
+    /*html*/
+    `
+    <div class='container'>
+        <div class="row mt-3">
+            <div class="col-lg-6">
+                <h3 style='margin:0px;' class='text-info'>Tarefas Completas</h3>
+            </div>
+        </div>
+        <hr class='bg-info'>
+    </div>
+
+    <div v-for='(task, index) in finishedtasks' :key='task.id' class='finished-task container row mt-2'>
+        <div>
+            <a href='#' class='text-success'>
+                <i class='fas fa-check mr-2'></i>
+            </a>
+        </div>
+        <div class='col-10 float-left'>
+            <h4>{{task.task_name}} - {{task.finish_date}}</h4>
+        </div>
+        <div class='col-1 float-right'>
+            <a href='#' class='text-danger' @click='unMark(task)'>
+                <i class='fas fa-minus' ></i>
+            </a>
+        </div>
+    </div>
+    `,
+    methods: {
+        unMark(task){
+            this.selectTask(task)
+            this.$emit('unmark', this.currentTask)
+        },
+        selectTask(task){
+            this.currentTask = task
+        }
+    },
+})
